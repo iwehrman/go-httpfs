f52e7d475c09efe3149e5b0a4c9986293a38c368
Ian Wehrman <wehrman@adobe.com>
2014-07-21 08:06:44 -0700

    Synchronize concurrent access to the conversion queue

--- convert/convert.go
+++ convert/convert.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os/exec"
 	"strconv"
+	"sync"
 )
 
 type thumbInfo struct {
@@ -16,21 +17,28 @@ type thumbInfo struct {
 
 const MAX_WORKING = 4
 
+var mutex = sync.RWMutex{}
 var queue = make([]string, 0)
 var working = make(map[string]bool)
 var waiting = make(map[string]*thumbInfo)
 
 func processQueue() {
+
+	mutex.Lock()
 	log.Print("Processing queue...")
 	key := queue[0]
 	queue = queue[1:]
 	working[key] = true
+	mutex.Unlock()
 
+	mutex.RLock()
 	if len(working) < MAX_WORKING && len(queue) > 0 {
 		go processQueue()
 	}
 
 	thumbInfo := waiting[key]
+	mutex.RUnlock()
+
 	dimAsStr := strconv.Itoa(thumbInfo.dimension)
 	dimensions := dimAsStr + "x" + dimAsStr
 	cmd := exec.Command("convert", "-thumbnail", dimensions, thumbInfo.fullPath, thumbInfo.thumbPath)
@@ -40,16 +48,25 @@ func processQueue() {
 		thumbInfo.notifier <- result
 	}
 
+	mutex.Lock()
 	delete(working, key)
 
 	if len(queue) > 0 {
 		go processQueue()
 	}
+	mutex.Unlock()
+
 }
 
 func enqueueThumbnailRequest(fullPath, thumbPath string, dimension int) <-chan error {
+
+	mutex.RLock()
+
 	var notifier chan error
 	if info, present := waiting[thumbPath]; !present {
+		mutex.RUnlock()
+		mutex.Lock()
+
 		log.Print("Initializing: " + thumbPath)
 		notifier = make(chan error, 1)
 		waiting[thumbPath] = &thumbInfo{
@@ -61,6 +78,8 @@ func enqueueThumbnailRequest(fullPath, thumbPath string, dimension int) <-chan e
 		}
 
 		queue = append(queue, thumbPath)
+		mutex.Unlock()
+		mutex.RLock()
 	} else {
 		log.Print("Updating: " + thumbPath)
 		info.callers = info.callers + 1
@@ -72,6 +91,7 @@ func enqueueThumbnailRequest(fullPath, thumbPath string, dimension int) <-chan e
 	if len(queue) == 1 {
 		go processQueue()
 	}
+	mutex.RUnlock()
 
 	return notifier
 }
